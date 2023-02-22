f7e1a4cd9f9ad7d17f6b26dbddd6f834ab0b5d1c
Abdulilah Al-shabany <alshabanyabdulilah2000@gmail.com>
2023-02-22 20:34:52 +0700

    first commit

---
+++ defaultParameters.js
@@ -0,0 +1,8 @@
+function welcome(
+    user = "Boy",
+    message = "Take of your health! treat yourself king"
+
+) {
+    alert(`Hi ${user}, ${message}`);
+}
+welcome();
---
+++ template.js
@@ -0,0 +1,7 @@
+let fname = "Abel";
+let lname = "Tesfaye";
+let age = prompt("Enter the Weekend's aka Abel Tesfaye age:");
+
+
+let result = `${fname} ${lname} is ${age} years old`;
+alert(result);
